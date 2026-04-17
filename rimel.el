35351a9aff2e9fb99f90180fdb2237616698b2ff
jixiuf <jixiuf@qq.com>
2026-04-17 17:54:43 +0800

    add a License header in rimel.el

--- rimel.el
+++ rimel.el
@@ -6,10 +6,22 @@
 ;; Package-Requires: ((emacs "29.4") (liberime "0.0.7"))
 ;; Keywords: convenience, Chinese, input-method, rime
 
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
